0a98ae7d0d51aae9c94a6a99041e85a8297ad007
Hyuk Kwon <82810259+poiu03290@users.noreply.github.com>
2022-01-27 23:49:32 +0900

    k번째 약수 문제(답변 추가 코드)

--- k번째 약수.py
+++ k번째 약수.py
@@ -38,3 +38,17 @@ if len(lst) >= m:
     print(lst[m-1])
 else:
     print("-1")
+
+# 답변
+# import sys
+# n, k = map(int, input().split())
+# cnt = 0
+
+# for i in range(1, n+1):
+#     if n % i == 0:
+#         cnt += 1
+#     if cnt == k:
+#         print(i)
+#         break
+# else:
+#     print(-1)

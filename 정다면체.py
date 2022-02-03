79506df3f7ce84dc620fc5386f822c9f2b5ad76a
Hyuk Kwon <82810259+poiu03290@users.noreply.github.com>
2022-02-03 22:43:12 +0900

    정다면체 문제

---
+++ 정다면체.py
@@ -0,0 +1,48 @@
+# 정다면체
+# 두 개의 정 N면체와 정 M면체의 두 개의 주사위를 던져서 나올 수 있는 눈의 합 중 
+# 가장 확률이 높은 숫자를 출력하는 프로그램을 작성하세요.
+# 정답이 여러 개일 경우 오름차순으로 출력합니다.
+
+# ▣ 입력설명
+# 첫 번째 줄에는 자연수 N과 M이 주어집니다. N과 M은 4, 6, 8, 12, 20 중의 하나입니다.
+
+# ▣ 출력설명
+# 첫 번째 줄에 답을 출력합니다.
+
+# ▣ 입력예제 1 
+# 4 6
+
+# ▣ 출력예제 1
+# 5 6 7
+
+# import sys
+# n, m = map(int, input().split())
+
+# cnt = [0] * 10
+
+# for i in range(n):
+#     for j in range(m):
+#         score = i + j
+
+# for idx, x in enumerate(cnt):
+#     if idx == score:
+#         x += 1
+#         print(cnt)
+
+
+import sys
+n, m = map(int, input().split())
+cnt = [0] * (n + m + 3)
+max = -2147000000
+
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        cnt[i+j] += 1
+
+for i in range(n+m+1):
+    if cnt[i] > max:
+        max = cnt[i]
+
+for i in range(n+m+1):
+    if cnt[i] == max:
+        print(i, end=' ')

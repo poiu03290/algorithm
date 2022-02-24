5e38e7b9a76e91d767a52db6f31c2e43f6b8e85c
Hyuk Kwon <82810259+poiu03290@users.noreply.github.com>
2022-02-25 03:41:31 +0900

    침몰하는 타이타닉 문제(그리디)

---
+++ 침몰하는 타이타닉.py
@@ -0,0 +1,58 @@
+# 침몰하는 타이타닉(그리디)
+# 유럽에서 가장 유명했던 유람선 타이타닉이 침몰하고 있습니다. 유람선에는 N명의 승객이 타고 있습니다. 
+# 구명보트를 타고 탈출해야 하는데 타이타닉에 있는 구명보트는 2명 이하로만 탈 수 있으며, 
+# 보트 한 개에 탈 수 있는 총 무게도 M kg 이하로 제한되어 있습니다.
+# N명의 승객 몸무게가 주어졌을 때 승객 모두가 탈출하기 위한 구명보트의 최소개수를 출력하는 프로그램을 작성하세요.
+
+# ▣ 입력설명
+# 첫째 줄에 자연수 N(5<=N<=1000)과 M(70<=M<=250)이 주어집니다.
+# 두 번째 줄에 N개로 구성된 몸무게 수열이 주어집니다. 몸무게는 50이상 150이하입니다.
+# 각 승객의 몸무게는 M을 넘지는 않습니다. 즉 탈출을 못하는 경우는 없습니다.
+
+# ▣ 출력설명
+# 첫째 줄에 구명보트의 최소 개수를 출력합니다.
+
+# ▣ 입력예제 1 
+# 5 140
+# 90 50 70 100 60
+
+# ▣ 출력예제 1
+# 3
+
+# 답변 (list 사용)
+# n, limit = map(int, input().split())
+# p = list(map(int, input().split()))
+# p.sort()
+# cnt = 0
+# while p:
+#     if len(p) == 1:
+#         cnt += 1
+#         break
+#     if p[0] + p[-1] > limit:
+#         cnt += 1
+#         p.pop()
+#     else:
+#         cnt += 1
+#         p.pop()
+#         p.pop(0)
+# print(cnt)
+
+# 답변 (deque 사용)
+from collections import deque
+n, limit = map(int, input().split())
+p = list(map(int, input().split()))
+p.sort()
+p = deque(p)  # list에서 pop(0)하면 하나씩 앞으로 오는 연산이되는데 비효율. deque를 쓰면 해결가능. 
+cnt = 0
+while p:
+    if len(p) == 1:
+        cnt += 1
+        break
+    if p[0] + p[-1] > limit:
+        cnt += 1
+        p.pop()
+    else:
+        cnt += 1
+        p.popleft()   # 데크에서 맨앞 숫자를 뺴는건 popleft
+        p.pop()
+print(cnt)
